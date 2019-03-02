a27925d66632580daf5aea467b4b18112640647b
avinash\avinash.t <qwe123kids@gmail.om>
2019-03-02 14:36:18 -0700

    spring microservice

---
+++ 3_Spring_Micro_Service/itemsharning-userservice/src/main/java/com/itemsharning/userservice/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.itemsharning.userservice.controller;
+
+import com.itemsharning.userservice.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("/{username}")
+    public User getUserByUserName(@PathVariable String username){
+        return userService.getUserByName(username);
+    }
+}
