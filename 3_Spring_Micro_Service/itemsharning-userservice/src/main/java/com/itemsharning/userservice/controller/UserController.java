a35ca529c39b1beeef34842cc97b66fba39adac0
avinash\avinash.t <qwe123kids@gmail.om>
2019-03-04 18:47:30 -0700

    microservice

--- 3_Spring_Micro_Service/itemsharning-userservice/src/main/java/com/itemsharning/userservice/controller/UserController.java
+++ 3_Spring_Micro_Service/itemsharning-userservice/src/main/java/com/itemsharning/userservice/controller/UserController.java
@@ -3,9 +3,7 @@ package com.itemsharning.userservice.controller;
 import com.itemsharning.userservice.model.User;
 import com.itemsharning.userservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/user")
@@ -16,5 +14,10 @@ public class UserController {
     @RequestMapping("/{username}")
     public User getUserByUserName(@PathVariable String username){
         return userService.getUserByName(username);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public User createUser(@RequestBody User user){
+        return userService.createUser(user);
     }
 }
--- 3_Spring_Micro_Service/itemsharning-userservice/src/main/java/com/itemsharning/userservice/service/impl/UserServiceImpl.java
+++ 3_Spring_Micro_Service/itemsharning-userservice/src/main/java/com/itemsharning/userservice/service/impl/UserServiceImpl.java
@@ -1,12 +1,19 @@
 package com.itemsharning.userservice.service.impl;
 
+import com.itemsharning.userservice.model.Role;
 import com.itemsharning.userservice.model.User;
+import com.itemsharning.userservice.model.UserRole;
 import com.itemsharning.userservice.repositry.UserRepositry;
 import com.itemsharning.userservice.service.UserService;
+import org.apache.catalina.security.SecurityUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -17,7 +24,26 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User createUser(User user) {
-        return null;
+
+        User localuser = userRepositry.findByUsername(user.getUsername());
+        if(localuser !=null){
+            LOG.info("User with username found {} ", user.getUsername());
+        }else{
+            Set<UserRole> userRoles=new HashSet<>();
+            Role localRole=new Role();
+            localRole.setRoleId(1);
+            userRoles.add(new UserRole(user,localRole));
+            user.getUserRole().addAll(userRoles);
+            Date today= new Date();
+            user.setJoinDate(today);
+
+            String encryptedPassword =  SecurityUtili
+            user.setPassword(encryptedPassword);
+            localuser = userRepositry.save(user);
+
+        }
+
+        return localuser;
     }
 
     @Override
