4d7043439036120539bc7d5d01e5fff336e123bf
AvinashTiwari85 <qwe123kids@gmail.com>
2019-05-12 12:27:07 -0600

    no message

---
+++ 5_MonoliticApp_To_Microservice/MonoliticApp/2_Transform_To_Microservice/rolling-stone-ecommerce-product-api/src/main/java/learn/avinash/spring/rollingstoneecommerceproductapi/RollingStoneEcommerceProductApiApplication.java
@@ -0,0 +1,13 @@
+package learn.avinash.spring.rollingstoneecommerceproductapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RollingStoneEcommerceProductApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RollingStoneEcommerceProductApiApplication.class, args);
+	}
+
+}
---
+++ 5_MonoliticApp_To_Microservice/MonoliticApp/2_Transform_To_Microservice/rolling-stone-ecommerce-product-api/src/test/java/learn/avinash/spring/rollingstoneecommerceproductapi/RollingStoneEcommerceProductApiApplicationTests.java
@@ -0,0 +1,16 @@
+package learn.avinash.spring.rollingstoneecommerceproductapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RollingStoneEcommerceProductApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
