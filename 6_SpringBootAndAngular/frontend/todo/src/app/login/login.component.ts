7062ab5574b5f004788a859df87581f51bf8f2d9
AvinashTiwari85 <qwe123kids@gmail.com>
2019-07-08 22:41:55 -0600

    no message

--- 6_SpringBootAndAngular/frontend/todo/src/app/login/login.component.ts
+++ 6_SpringBootAndAngular/frontend/todo/src/app/login/login.component.ts
@@ -9,13 +9,22 @@ export class LoginComponent implements OnInit {
 
   username = 'Avinash'
   password = ''
+  errorMessage ='Invalid credentials'
+  invalidLogin = false
   constructor() { }
 
   ngOnInit() {
   }
 
   handleLogin(){
+    if(this.username === 'Avinash' && this.password === 'dummy'){
+      this.invalidLogin = false;
+    }else{
+      this.invalidLogin = true
+    }
     console.log(this.username)
+    console.log(this.password)
   }
+
 
 }
