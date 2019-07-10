7f597e6e643bd7dc97566629f8cd889031faeab4
AvinashTiwari85 <qwe123kids@gmail.com>
2019-07-09 22:40:00 -0600

    no message

--- 6_SpringBootAndAngular/frontend/todo/src/app/login/login.component.ts
+++ 6_SpringBootAndAngular/frontend/todo/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,13 +12,14 @@ export class LoginComponent implements OnInit {
   password = ''
   errorMessage ='Invalid credentials'
   invalidLogin = false
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
   handleLogin(){
     if(this.username === 'Avinash' && this.password === 'dummy'){
+      this.router.navigate(['welcome']);
       this.invalidLogin = false;
     }else{
       this.invalidLogin = true
