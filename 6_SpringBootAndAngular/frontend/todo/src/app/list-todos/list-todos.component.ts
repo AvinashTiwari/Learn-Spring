db348afce7afc589d9d7b0b37072fb33fd31bdd3
AvinashTiwari85 <qwe123kids@gmail.com>
2019-07-11 22:40:53 -0600

    no message

---
+++ 6_SpringBootAndAngular/frontend/todo/src/app/list-todos/list-todos.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-todos',
+  templateUrl: './list-todos.component.html',
+  styleUrls: ['./list-todos.component.css']
+})
+export class ListTodosComponent implements OnInit {
+
+  todos =[{
+    id: 1,
+    description: 'Learn to Dance'
+
+  },
+  {
+    id: 2,
+    description: 'Expert'
+
+  },
+  {
+    id: 3,
+    description: 'UK, Europe'
+
+  }]
+  todo ={
+    id: 1,
+    description: 'Learn to Dance'
+
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
