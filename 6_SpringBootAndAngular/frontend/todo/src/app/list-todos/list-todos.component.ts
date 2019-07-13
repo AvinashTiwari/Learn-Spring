5656d78aea5917855a23f3267f1b0263ad14f625
AvinashTiwari85 <qwe123kids@gmail.com>
2019-07-13 17:07:25 -0600

    no message

--- 6_SpringBootAndAngular/frontend/todo/src/app/list-todos/list-todos.component.ts
+++ 6_SpringBootAndAngular/frontend/todo/src/app/list-todos/list-todos.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Todo{
+  constructor(
+    public id: number,
+    public description:string,
+    public done: boolean,
+    public targetDate:Date
+  ){
+
+  }
+}
+
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
@@ -7,21 +18,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListTodosComponent implements OnInit {
 
-  todos =[{
-    id: 1,
-    description: 'Learn to Dance'
-
-  },
-  {
-    id: 2,
-    description: 'Expert'
-
-  },
-  {
-    id: 3,
-    description: 'UK, Europe'
-
-  }]
+  todos =[
+    new Todo(1,'Learn to Dance',false, new Date()),
+    new Todo(2,'Expert',false, new Date()),
+    new Todo(3,'vist UK, Europe',false, new Date())
+  ]
   todo ={
     id: 1,
     description: 'Learn to Dance'
