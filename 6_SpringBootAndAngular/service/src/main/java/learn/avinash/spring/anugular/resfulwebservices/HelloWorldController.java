67de6f394c84c732b0bb7d64af4ae6b02bc258ad
AvinashTiwari85 <qwe123kids@gmail.com>
2019-07-28 16:34:16 -0600

    no message

---
+++ 6_SpringBootAndAngular/service/src/main/java/learn/avinash/spring/anugular/resfulwebservices/HelloWorldController.java
@@ -0,0 +1,15 @@
+package learn.avinash.spring.anugular.resfulwebservices;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping( path="/hello-world")
+    public String HelloWorld(){
+        return  "Hello World";
+    }
+}
---
+++ 6_SpringBootAndAngular/service/src/main/java/learn/avinash/spring/anugular/resfulwebservices/ResfulWebservicesApplication.java
@@ -0,0 +1,13 @@
+package learn.avinash.spring.anugular.resfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResfulWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResfulWebservicesApplication.class, args);
+	}
+
+}
