ef625110891c39dfb3c7ac743317ba68bef5a047
AvinashTiwari85 <qwe123kids@gmail.com>
2019-08-01 22:24:05 -0600

    no message

--- 6_SpringBootAndAngular/service/src/main/java/learn/avinash/spring/anugular/resfulwebservices/HelloWorldController.java
+++ 6_SpringBootAndAngular/service/src/main/java/learn/avinash/spring/anugular/resfulwebservices/HelloWorldController.java
@@ -12,4 +12,9 @@ public class HelloWorldController {
     public String HelloWorld(){
         return  "Hello World";
     }
+
+    @GetMapping( path="/hello-world-bean")
+    public HelloWorldBean HelloWorldBean(){
+        return  new HelloWorldBean("Hello World");
+    }
 }
