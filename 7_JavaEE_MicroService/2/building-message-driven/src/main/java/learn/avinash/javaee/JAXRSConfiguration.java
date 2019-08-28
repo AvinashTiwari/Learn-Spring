124590f232566cc10ed089908db7b30b7e970d67
AvinashTiwari85 <qwe123kids@gmail.com>
2019-08-28 17:45:17 -0600

    no message

---
+++ 7_JavaEE_MicroService/2/building-message-driven/src/main/java/learn/avinash/javaee/JAXRSConfiguration.java
@@ -0,0 +1,11 @@
+package learn.avinash.javaee;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures a JAX-RS endpoint.
+ */
+@ApplicationPath("api")
+public class JAXRSConfiguration extends Application {
+}
