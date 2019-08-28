b72bfd0eaedc23a46c7b8ed2deb7eeab9cd93fac
AvinashTiwari85 <qwe123kids@gmail.com>
2019-08-27 19:47:12 -0600

    no message

---
+++ 7_JavaEE_MicroService/api-documentation/src/main/java/package-info.java
@@ -0,0 +1,7 @@
+import org.eclipse.microprofile.openapi.annotations.ExternalDocumentation;
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Contact;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+import org.eclipse.microprofile.openapi.annotations.info.License;
+import org.eclipse.microprofile.openapi.annotations.servers.Server;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
