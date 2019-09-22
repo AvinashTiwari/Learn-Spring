4395dd321d0c5a948b792e2863251d793a1be522
AvinashTiwari85 <qwe123kids@gmail.com>
2019-09-22 16:35:16 -0600

    no message

--- 6_SpringBootAndAngular/frontend/todo/src/app/service/data/todo-data.service.ts
+++ 6_SpringBootAndAngular/frontend/todo/src/app/service/data/todo-data.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { Todo } from 'src/app/list-todos/list-todos.component';
-import { API_URL } from 'src/app/app.constants';
+import { API_URL, API_JPA_URL } from 'src/app/app.constants';
 @Injectable({
   providedIn: 'root'
 })
@@ -10,23 +10,23 @@ export class TodoDataService {
   constructor(private http: HttpClient) { }
 
   retriveAllTodos(username){
-    return this.http.get<Todo[]>(`${API_URL}/users/${username}/todos`)
+    return this.http.get<Todo[]>(`${API_JPA_URL}/users/${username}/todos`)
     //console.log("Execute hello World Service");
   }
 
   deleteTodo(username, id){
-    return this.http.delete<Todo[]>(`${API_URL}/users/${username}/todos/${id}`)
+    return this.http.delete<Todo[]>(`${API_JPA_URL}/users/${username}/todos/${id}`)
   }
 
   retriveTodo(username, id){
-    return this.http.get<Todo>(`${API_URL}/users/${username}/todos/${id}`)
+    return this.http.get<Todo>(`${API_JPA_URL}/users/${username}/todos/${id}`)
   }
 
   updateTodo(username, id, todo){
-    return this.http.put(`${API_URL}/users/${username}/todos/${id}`,todo)
+    return this.http.put(`${API_JPA_URL}/users/${username}/todos/${id}`,todo)
   }
 
   createTodo(username,  todo){
-    return this.http.post(`${API_URL}/users/${username}/todos`,todo)
+    return this.http.post(`${API_JPA_URL}/users/${username}/todos`,todo)
   }
 }
--- 6_SpringBootAndAngular/service/src/main/resources/data.sql
+++ 6_SpringBootAndAngular/service/src/main/resources/data.sql
@@ -1,8 +1,8 @@
-/*insert into todo(id, username,description,target_date,is_done)
-values(10001, 'avinash', 'Learn JPA', '04-13-20019', 0);
+insert into todo(id, username,description,target_date,is_done)
+values(10001, 'avinash', 'Learn JPA', sysdate(), false);
 
 insert into todo(id, username,description,target_date,is_done)
-values(10002, 'avinash', 'Learn Data JPA', '04-13-20019', 0);
+values(10002, 'avinash', 'Learn Data JPA', sysdate(), false);
 
 insert into todo(id, username,description,target_date,is_done)
-values(10003, 'avinash', 'Learn Microservices', '04-13-20019', 0);*/
+values(10003, 'avinash', 'Learn Microservices', sysdate(), false);
