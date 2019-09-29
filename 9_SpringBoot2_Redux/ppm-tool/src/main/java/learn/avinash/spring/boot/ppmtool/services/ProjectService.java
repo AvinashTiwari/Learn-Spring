2faf5aedba705622cd9373edda04beadb40815a9
AvinashTiwari85 <qwe123kids@gmail.com>
2019-09-28 18:08:47 -0600

    no message

--- 9_SpringBoot2_Redux/ppm-tool/src/main/java/learn/avinash/spring/boot/ppmtool/services/ProjectService.java
+++ 9_SpringBoot2_Redux/ppm-tool/src/main/java/learn/avinash/spring/boot/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package learn.avinash.spring.boot.ppmtool.services;
 
 import learn.avinash.spring.boot.ppmtool.domain.Project;
+import learn.avinash.spring.boot.ppmtool.exceptions.ProjectIdException;
 import learn.avinash.spring.boot.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,9 +14,29 @@ public class ProjectService {
 
     public Project saveOrUpdateProject(Project project){
 
-        //Logic
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch (Exception e){
+            throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+"' already exists");
+        }
+    }
 
-        return projectRepository.save(project);
+    public Project findProjectByIdentifier(String projectId){
+
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if(project == null){
+            throw new ProjectIdException("Project ID '"+projectId+"' does not exist");
+
+        }
+
+
+        return project;
+    }
+
+    public Iterable<Project> findAllProjects(){
+        return projectRepository.findAll();
     }
 
 }
