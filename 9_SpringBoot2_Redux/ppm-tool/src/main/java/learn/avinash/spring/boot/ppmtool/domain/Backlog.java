150f39f2118f011125f1cf4efd3167bfe617a140
AvinashTiwari85 <qwe123kids@gmail.com>
2019-10-16 22:27:44 -0600

    no message

--- 9_SpringBoot2_Redux/ppm-tool/src/main/java/learn/avinash/spring/boot/ppmtool/domain/Backlog.java
+++ 9_SpringBoot2_Redux/ppm-tool/src/main/java/learn/avinash/spring/boot/ppmtool/domain/Backlog.java
@@ -1,8 +1,9 @@
 package learn.avinash.spring.boot.ppmtool.domain;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Backlog {
@@ -20,6 +21,8 @@ public class Backlog {
     private Project project;
 
     //OneToMany projecttasks
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "backlog")
+    private List<ProjectTask> projectTasks = new ArrayList<>();
 
 
     public Backlog() {
@@ -55,5 +58,13 @@ public class Backlog {
 
     public void setProject(Project project) {
         this.project = project;
+    }
+
+    public List<ProjectTask> getProjectTasks() {
+        return projectTasks;
+    }
+
+    public void setProjectTasks(List<ProjectTask> projectTasks) {
+        this.projectTasks = projectTasks;
     }
 }
