4b31b50edafae470e8c06a9f06740a8f28302ad6
AvinashTiwari85 <qwe123kids@gmail.com>
2019-11-10 14:21:54 -0700

    no message

---
+++ 10_Spring_Project/mssc-brewy-project/src/main/java/learn/avinash/spring/msscbrewy/MsscBrewyApplication.java
@@ -0,0 +1,13 @@
+package learn.avinash.spring.msscbrewy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBrewyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBrewyApplication.class, args);
+	}
+
+}
---
+++ 10_Spring_Project/mssc-brewy-project/src/test/java/learn/avinash/spring/msscbrewy/MsscBrewyApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.avinash.spring.msscbrewy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBrewyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
