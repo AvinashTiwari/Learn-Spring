fd056a655ea1638a9040f98d9a176cc2a4fa564c
AvinashTiwari85 <qwe123kids@gmail.com>
2019-11-20 18:56:10 -0700

    no message

---
+++ 10_Spring_Project/2_mssc-beer-service/src/test/java/learn/avinsh/spring/mssc/beer/service/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.avinsh.spring.mssc.beer.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
