358b3958dea92c595408ffc4c04e9116e7ed4005
AvinashTiwari85 <qwe123kids@gmail.com>
2020-01-04 14:09:21 -0700

    no message

---
+++ 10_Spring_Project/5_mssc-jackson-json/src/main/java/learn/avinash/jackson/json/learnjacksonjson/LearnJacksonJsonApplication.java
@@ -0,0 +1,13 @@
+package learn.avinash.jackson.json.learnjacksonjson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJacksonJsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJacksonJsonApplication.class, args);
+	}
+
+}
---
+++ 10_Spring_Project/5_mssc-jackson-json/src/test/java/learn/avinash/jackson/json/learnjacksonjson/LearnJacksonJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.avinash.jackson.json.learnjacksonjson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJacksonJsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
