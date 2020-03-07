8f3c77e41c6e3780bbde9ce825790a251d7443e8
AvinashTiwari85 <qwe123kids@gmail.com>
2020-03-07 15:10:02 -0700

    no message

---
+++ 14_SpringStateMachince/ssm/src/test/java/learn/avinash/spring/state/machince/ssm/SsmApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.avinash.spring.state.machince.ssm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
