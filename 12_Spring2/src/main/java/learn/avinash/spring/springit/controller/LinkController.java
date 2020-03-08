0fb7334c61d1db8dbfbcf614619664627e950b3f
AvinashTiwari85 <qwe123kids@gmail.com>
2020-03-08 16:37:03 -0600

    no message

---
+++ 12_Spring2/src/main/java/learn/avinash/spring/springit/controller/LinkController.java
@@ -0,0 +1,44 @@
+package learn.avinash.spring.springit.controller;
+
+import learn.avinash.spring.springit.domain.Link;
+import learn.avinash.spring.springit.repository.LinkedRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/links")
+public class LinkController {
+
+    private LinkedRepository linkedRepository;
+
+    public LinkController(LinkedRepository linkedRepository) {
+        this.linkedRepository = linkedRepository;
+    }
+
+    @GetMapping("/")
+    public List<Link> list(){
+        return linkedRepository.findAll();
+    }
+
+    @PostMapping("/create")
+    public Link create(@ModelAttribute Link link){
+        return linkedRepository.save(link);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Link> read(@PathVariable Long id){
+        return linkedRepository.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public  Link update(@PathVariable Long id, @ModelAttribute Link link){
+        return linkedRepository.save(link);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id){
+         linkedRepository.deleteById(id);
+    }
+}
