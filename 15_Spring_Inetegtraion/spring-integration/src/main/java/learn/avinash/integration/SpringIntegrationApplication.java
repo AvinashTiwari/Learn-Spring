dc51870004a2e4e623ddad96ebdf655c299b2f61
AvinashTiwari85 <qwe123kids@gmail.com>
2020-05-01 18:57:38 -0600

    no message

--- 15_Spring_Inetegtraion/spring-integration/src/main/java/learn/avinash/integration/SpringIntegrationApplication.java
+++ 15_Spring_Inetegtraion/spring-integration/src/main/java/learn/avinash/integration/SpringIntegrationApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @Configuration
+@ImportResource("integration-context.xml")
 public class SpringIntegrationApplication implements ApplicationRunner {
 
 	public static void main(String[] args) {
@@ -17,6 +19,7 @@ public class SpringIntegrationApplication implements ApplicationRunner {
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
 		// TODO Auto-generated method stub
+		System.out.println("Hello World");
 		
 	}
 
