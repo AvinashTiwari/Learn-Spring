11b948c06ebc2c49fa7dde115a8e83adc1556e73
AvinashTiwari85 <qwe123kids@gmail.com>
2020-05-03 13:38:16 -0600

    no message

--- 15_Spring_Inetegtraion/sspring-integration-simple-project/src/main/java/learn/avinash/integration/PrintService.java
+++ 15_Spring_Inetegtraion/sspring-integration-simple-project/src/main/java/learn/avinash/integration/PrintService.java
@@ -1,11 +1,21 @@
 package learn.avinash.integration;
 
+import java.util.Map.Entry;
+
 import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
 
 public class PrintService {
 	
 	public void print(Message<String> message) {
-		System.out.print(message.getPayload());
+		MessageHeaders headers = message.getHeaders();
+		
+		for(Entry<String, Object> enter: headers.entrySet()) {
+			System.out.println(enter.getKey());
+			System.out.println(enter.getValue());			
+		}
+		
+		System.out.println(message.getPayload());
 	}
 
 }
--- 15_Spring_Inetegtraion/sspring-integration-simple-project/src/main/java/learn/avinash/integration/SspringIntegrationSimpleProjectApplication.java
+++ 15_Spring_Inetegtraion/sspring-integration-simple-project/src/main/java/learn/avinash/integration/SspringIntegrationSimpleProjectApplication.java
@@ -1,5 +1,8 @@
 package learn.avinash.integration;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
@@ -7,7 +10,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.support.GenericMessage;
+import org.springframework.integration.support.MessageBuilder;
 
 @SpringBootApplication
 @Configuration
@@ -18,14 +23,35 @@ public class SspringIntegrationSimpleProjectApplication implements ApplicationRu
 		SpringApplication.run(SspringIntegrationSimpleProjectApplication.class, args);
 	}
 	
+	
+	
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
 		// TODO Auto-generated method stub
-		Message message = new GenericMessage<String>("Hello World");
+		
+
+		Message<String> message = MessageBuilder.withPayload("Hello world from builder pattern ")
+				.setHeader("NewHeader", "New heder value").build();
+		
 		PrintService ps = new PrintService();
 		ps.print(message);
 		
 	}
+
+	
+	/*
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Map<String , Object> map = new HashMap<String, Object>();
+		map.put("key", "value");
+		MessageHeaders headers =  new MessageHeaders(map);
+		Message message = new GenericMessage<String>("Hello World",headers);
+		PrintService ps = new PrintService();
+		ps.print(message);
+		
+	}*/
 
 
 }
