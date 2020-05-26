b988944c721e42cd8900efe76bc1e574dab7c2b4
AvinashTiwari85 <qwe123kids@gmail.com>
2020-05-26 17:07:56 -0600

    no message

---
+++ 15_Spring_Inetegtraion/14-spring-integration-compt-splitter/src/main/java/learn/avinash/integration/PrinterGateway.java
@@ -0,0 +1,11 @@
+package learn.avinash.integration;
+
+import java.util.concurrent.Future;
+
+import org.springframework.messaging.Message;
+
+public interface PrinterGateway {
+	
+	public Future<Message<String>> print(Message<?> message);
+
+}
