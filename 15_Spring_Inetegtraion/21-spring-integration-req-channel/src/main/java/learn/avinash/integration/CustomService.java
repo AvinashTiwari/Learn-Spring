f886b93d92c9128ac569ad08ca41ccf6fa2fdcb1
AvinashTiwari85 <qwe123kids@gmail.com>
2020-06-06 18:09:35 -0600

    no message

---
+++ 15_Spring_Inetegtraion/21-spring-integration-req-channel/src/main/java/learn/avinash/integration/CustomService.java
@@ -0,0 +1,9 @@
+package learn.avinash.integration;
+
+public class CustomService {
+	
+	public String getPhoneNumber(Person person) {
+		return  "888-888-888";
+	}
+
+}
---
+++ 15_Spring_Inetegtraion/21-spring-integration-req-channel/src/main/java/learn/avinash/integration/Person.java
@@ -0,0 +1,45 @@
+package learn.avinash.integration;
+
+public class Person {
+	private String firstName;
+	private String lastName;
+	private String phoneNumber;
+	
+	
+	public Person(String firstName, String lastName) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	/*
+	@Override
+	public String toString() {
+		return "Person [firstName=" + firstName + ", lastName=" + lastName + "]";
+	}
+	*/
+	
+	
+
+}
