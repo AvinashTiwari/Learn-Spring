04903c25ed0313fc6d47e9b8832647faab58530a
AvinashTiwari85 <qwe123kids@gmail.com>
2020-07-07 17:16:21 -0600

    no message

---
+++ 16_Spring_Boot_Camel/1_Introduction/src/main/java/learn/avinash/camel/file/CopyFileCamelWithoutNoop.java
@@ -0,0 +1,27 @@
+package learn.avinash.camel.file;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class CopyFileCamelWithoutNoop {
+    public static void main(String[] args) {
+        CamelContext context = new DefaultCamelContext();
+        try {
+            context.addRoutes(new RouteBuilder() {
+                @Override
+                public void configure() throws Exception {
+
+                    from("file:data/input")
+                            .to("file:data/output");
+
+                }
+            });
+            context.start();
+            Thread.sleep(5000);
+            context.stop();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
