48a5765cffe8a71ac336cc5aa2989759cbe7264f
AvinashTiwari85 <qwe123kids@gmail.com>
2020-07-11 16:53:12 -0600

    no message

---
+++ 16_Spring_Boot_Camel/1_Introduction/src/main/java/learn/avinash/camel/file/CopyFileCamelLogging.java
@@ -0,0 +1,28 @@
+package learn.avinash.camel.file;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class CopyFileCamelLogging {
+    public static void main(String[] args) {
+        CamelContext context = new DefaultCamelContext();
+        try {
+            context.addRoutes(new RouteBuilder() {
+                @Override
+                public void configure() throws Exception {
+
+                    from("file:data/input?noop=true")
+                            .to("log:?Level=INFO&showBody=True")
+                            .to("file:data/output");
+
+                }
+            });
+            context.start();
+            Thread.sleep(5000);
+            context.stop();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
