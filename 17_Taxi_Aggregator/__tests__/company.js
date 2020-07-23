66c8d859071bca5bc5df7d833a8a67e2a3c23c4b
AvinashTiwari85 <qwe123kids@gmail.com>
2020-07-23 17:42:48 -0600

    no message

---
+++ 17_Taxi_Aggregator/__tests__/company.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const Company = require("../models/Company");
+
+describe("company test", () => {
+  test("first test", () => {
+    expect("name").toBe("name");
+  });
+});
