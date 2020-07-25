6860115cb84984fb5041619a38ce52e70d60222a
AvinashTiwari85 <qwe123kids@gmail.com>
2020-07-25 17:11:38 -0600

    no message

--- 17_Taxi_Aggregator/__tests__/company.js
+++ 17_Taxi_Aggregator/__tests__/company.js
@@ -1,8 +1,30 @@
 const mongoose = require("mongoose");
 const Company = require("../models/Company");
 
+beforeAll(() => {
+  mongoose.Promise = global.Promise;
+  mongoose.connect("mongodb://localhost/taxi-aggregator", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  });
+});
+
+beforeEach(() => {});
+afterEach(async () => {
+  await Company.deleteMany();
+});
+
+afterAll((done) => {
+  mongoose.disconnect(done);
+});
+
 describe("company test", () => {
-  test("first test", () => {
-    expect("name").toBe("name");
+  test("create company", async () => {
+    let compnay = new Company();
+    compnay.name = "First Company";
+    compnay = await compnay.save();
+
+    const count = await Company.countDocuments();
+    expect(count).toBe(1);
   });
 });
