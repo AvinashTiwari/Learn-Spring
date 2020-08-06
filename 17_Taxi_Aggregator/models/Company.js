b1e5f3bd50f47be1599dcb0dc8c1cb359b3d4569
AvinashTiwari85 <qwe123kids@gmail.com>
2020-08-06 15:38:36 -0600

    no message

--- 17_Taxi_Aggregator/models/Company.js
+++ 17_Taxi_Aggregator/models/Company.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 
 const CompanySchema = new Schema({
   name: { type: String, require: true },
+  taxies: [{ type: Schema.Types.ObjectId, ref: "taxi" }],
 });
 
 module.exports = mongoose.model("company", CompanySchema);
