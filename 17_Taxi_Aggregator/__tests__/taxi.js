6885d8188f6b95f46c4e19cde4243ec2402acd6f
AvinashTiwari85 <qwe123kids@gmail.com>
2020-08-08 15:03:25 -0600

    no message

--- 17_Taxi_Aggregator/__tests__/taxi.js
+++ 17_Taxi_Aggregator/__tests__/taxi.js
@@ -31,4 +31,35 @@ describe("Taxi test", () => {
     const readTaxi = await Taxi.findOne();
     expect(readTaxi.owner.name).toBe("Driver 1");
   });
+
+  test("Populate References", async () => {
+    let company = new Company();
+    company.name = "First Company";
+    company = await company.save();
+
+    let taxi1 = new Taxi();
+    taxi1.brand = "Toyota";
+    taxi1.model = "Yaris";
+    taxi1.year = 2020;
+    taxi1.owner = { name: "Driver 1", experience: 15 };
+    taxi1 = await taxi1.save();
+
+    let taxi2 = new Taxi();
+    taxi2.brand = "Beze";
+    taxi2.model = "Class E";
+    taxi2.year = 2020;
+    taxi2.owner = { name: "Driver 7", experience: 7 };
+    taxi2 = await taxi2.save();
+
+    company.taxies = [taxi1.id, taxi2.id];
+    company = await company.save();
+    console.log(company);
+
+    const readCompany = await Company.findById(company.id);
+
+    console.log(readCompany.populate());
+
+    //expect(readCompany.taxis[0].model).toBe("Yaris");
+    //expect(readCompany.taxis[1].model).toBe("Class E");
+  });
 });
