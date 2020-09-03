ac190973d20eeabbd424664fd46a5849e81f22e7
AvinashTiwari85 <qwe123kids@gmail.com>
2020-09-03 12:57:29 -0600

    no message

---
+++ 16_Spring_Boot_Camel/17_SpringBoot_Camel_InputValidation/src/main/java/com/learncamel/routes/KafkaRoute.java
@@ -0,0 +1,65 @@
+package com.learncamel.routes;
+
+import com.learncamel.alert.MailProcessor;
+import com.learncamel.domain.Item;
+import com.learncamel.exception.DataException;
+import com.learncamel.processor.ValidateDataProcessor;
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.Predicate;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.gson.GsonDataFormat;
+import org.postgresql.util.PSQLException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by z001qgd on 1/3/18.
+ */
+@Component
+public class KafkaRoute extends RouteBuilder{
+
+    @Autowired
+    Environment environment;
+
+    @Qualifier("dataSource")
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    MailProcessor mailProcessor;
+
+    @Autowired
+    ValidateDataProcessor validateDataProcessor;
+
+
+    @Override
+    public void configure() throws Exception {
+
+        Predicate isNotMock  = header("env").isNotEqualTo("mock");
+
+        GsonDataFormat itemFormat = new GsonDataFormat(Item.class);
+
+        onException(PSQLException.class).log(LoggingLevel.ERROR,"PSQLException in the route ${body}")
+                .maximumRedeliveries(3).redeliveryDelay(3000).backOffMultiplier(2).retryAttemptedLogLevel(LoggingLevel.ERROR);
+
+        onException(DataException.class,RuntimeException.class).log(LoggingLevel.ERROR, "DataException in the route ${body}")
+                .choice()
+                .when(isNotMock)
+                    .process(mailProcessor)
+                .end()
+                .to("{{errorRoute}}");
+
+
+        from("{{fromRoute}}")
+                    .log("Read Message from Kafka ${body}")
+                .unmarshal(itemFormat)
+                .log("Unmarshalled message is ${body}")
+                .process(validateDataProcessor)
+                .to("{{toRoute}}");
+
+        }
+}
