0e4c6c9ae20285dce2205a92eeb1b26048eae8fa
AvinashTiwari85 <qwe123kids@gmail.com>
2021-02-12 16:53:05 -0700

    no message

---
+++ 21_Java_interview/src/main/java/findvowel/findvowelsolone.java
@@ -0,0 +1,16 @@
+package findvowel;
+
+public class findvowelsolone {
+    public static void main(String[] args) {
+        vowels();
+    }
+
+   public static  void vowels(){
+       String str = new String("Avinash Tiwari");
+       for(int i=0; i<str.length(); i++) {
+           if(str.charAt(i) == 'a'|| str.charAt(i) == 'e'|| str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u') {
+               System.out.println("Given string contains "+str.charAt(i)+" at the index "+i);
+           }
+       }
+    }
+}
