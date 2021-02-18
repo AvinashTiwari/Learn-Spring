06ef8ec8fe6532950f76e7c105f8f54440c43cdf
AvinashTiwari85 <qwe123kids@gmail.com>
2021-02-18 16:32:07 -0700

    no message

---
+++ 21_Java_interview/src/main/java/middlepoint/MiddlePointSol.java
@@ -0,0 +1,59 @@
+package middlepoint;
+
+public class MiddlePointSol {
+    Node head;
+
+    class Node
+    {
+        int data;
+        Node next;
+        Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+
+    public void push(int new_data)
+    {
+        Node new_node = new Node(new_data);
+        head = new_node;
+    }
+
+    void printMiddle()
+    {
+        Node slow_ptr = head;
+        Node fast_ptr = head;
+        if (head != null)
+        {
+            while (fast_ptr != null && fast_ptr.next != null)
+            {
+                fast_ptr = fast_ptr.next.next;
+                slow_ptr = slow_ptr.next;
+            }
+            System.out.println("The middle element is [" +
+                    slow_ptr.data + "] \n");
+        }
+    }
+
+    public void printList()
+    {
+        Node tnode = head;
+        while (tnode != null)
+        {
+            System.out.print(tnode.data+"->");
+            tnode = tnode.next;
+        }
+        System.out.println("NULL");
+    }
+    public static void main(String [] args)
+    {
+        MiddlePointSol llist = new MiddlePointSol();
+        for (int i=5; i>0; --i)
+        {
+            llist.push(i);
+            llist.printList();
+            llist.printMiddle();
+        }
+    }
+}
