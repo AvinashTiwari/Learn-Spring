0688621a0a60ead822f207bfe04ece2197932208
AvinashTiwari85 <qwe123kids@gmail.com>
2021-02-17 20:00:13 -0700

    no message

---
+++ 21_Java_interview/src/main/java/datastruct/node/App.java
@@ -0,0 +1,37 @@
+package datastruct.node;
+
+public class App {
+    public static void main(String[] args) {
+        Node nodea = new Node();
+        nodea.data = 4;
+
+        Node nodeb = new Node();
+        nodeb.data = 3;
+
+
+        Node nodec = new Node();
+        nodec.data = 7;
+
+        Node noded = new Node();
+        noded.data = 8;
+
+
+        nodea.next = nodeb;
+        nodeb.next = nodec;
+        nodec.next = noded;
+
+        System.out.println(listLength(nodea));
+    }
+
+    public static int listLength(Node node){
+        int length =0;
+        Node currentNode = node;
+        while(currentNode != null){
+           
+            length++;
+            currentNode = currentNode.next;
+        }
+
+        return  length;
+    }
+}
---
+++ 21_Java_interview/src/main/java/datastruct/node/Node.java
@@ -0,0 +1,6 @@
+package datastruct.node;
+
+public class Node {
+    int data;
+    Node next = null;
+}
