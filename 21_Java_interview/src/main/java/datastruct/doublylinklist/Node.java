f56fee0f7c3e9342ccbacc7b9d793b477eaa121f
AvinashTiwari85 <qwe123kids@gmail.com>
2021-02-23 12:49:48 -0700

    no message

---
+++ 21_Java_interview/src/main/java/datastruct/doublylinklist/Node.java
@@ -0,0 +1,11 @@
+package datastruct.doublylinklist;
+
+public class Node {
+    int data;
+    Node next ;
+    Node previous;
+    public void displayNode(){
+        System.out.println("{ " + data + " }");
+
+    }
+}
