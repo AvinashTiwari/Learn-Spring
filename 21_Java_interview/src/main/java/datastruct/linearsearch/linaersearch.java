b910649f3d61b62e66c2fb14457b59e941d49952
AvinashTiwari85 <qwe123kids@gmail.com>
2021-02-28 14:27:05 -0700

    no message

---
+++ 21_Java_interview/src/main/java/datastruct/linearsearch/linaersearch.java
@@ -0,0 +1,19 @@
+package datastruct.linearsearch;
+
+public class linaersearch {
+    public static void main(String[] args) {
+
+    }
+
+    public static int linearSearch(int [] a, int x){
+
+        for(int i =0;  i < a.length; i++){
+            if(a[i] == x){
+              return i;
+            }
+        }
+
+
+        return  -1;
+    }
+}
