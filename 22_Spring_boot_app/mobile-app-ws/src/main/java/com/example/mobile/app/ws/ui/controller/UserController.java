f0a04b9738335876c86fbd77e8ba39c0fd15d68c
AvinashTiwari85 <qwe123kids@gmail.com>
2021-03-11 13:25:11 -0700

    no message

--- 22_Spring_boot_app/mobile-app-ws/src/main/java/com/example/mobile/app/ws/ui/controller/UserController.java
+++ 22_Spring_boot_app/mobile-app-ws/src/main/java/com/example/mobile/app/ws/ui/controller/UserController.java
@@ -1,9 +1,27 @@
 package com.example.mobile.app.ws.ui.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("users")
 public class UserController {
+    @GetMapping
+    public String getUser(){
+        return "get user was called";
+    }
+
+    @PostMapping
+    public String createUser(){
+        return "Create user was called";
+    }
+
+    @PutMapping
+    public  String updateUser(){
+        return "update Usr was called";
+    }
+
+    @DeleteMapping
+    public String deleteUser(){
+        return "delete User was called";
+    }
 }
