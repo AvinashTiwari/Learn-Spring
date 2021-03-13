57f68b2b14f4dced54df8bb7b9bba34ca6285298
AvinashTiwari85 <qwe123kids@gmail.com>
2021-03-12 21:05:47 -0700

    no message

--- 21_Java_interview/src/main/java/datastruct/mergesort/App.java
+++ 21_Java_interview/src/main/java/datastruct/mergesort/App.java
@@ -1,4 +1,12 @@
 package datastruct.mergesort;
 
 public class App {
+    public static void main(String[] args) {
+        int[] inputArray = {9,7,3,1,6,3,2,6,8,9,2,3,0};
+        MergeSort sorter = new MergeSort();
+        sorter.sort(inputArray);
+        for(int i=0; i < inputArray.length; i++){
+            System.out.println(inputArray[i]);
+        }
+    }
 }
