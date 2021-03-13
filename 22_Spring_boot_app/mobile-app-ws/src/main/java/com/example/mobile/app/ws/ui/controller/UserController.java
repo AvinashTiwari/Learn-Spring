adcccc215b4f176d92f68403f90900c06da798e4
AvinashTiwari85 <qwe123kids@gmail.com>
2021-03-13 15:56:07 -0700

    no message

--- 22_Spring_boot_app/mobile-app-ws/src/main/java/com/example/mobile/app/ws/ui/controller/UserController.java
+++ 22_Spring_boot_app/mobile-app-ws/src/main/java/com/example/mobile/app/ws/ui/controller/UserController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("users")
 public class UserController {
-    @GetMapping
-    public String getUser(){
-        return "get user was called";
+    @GetMapping(path="/{userId}")
+    public String getUser(@PathVariable String userId){
+        return "get user was userrid " + userId;
     }
 
     @PostMapping
