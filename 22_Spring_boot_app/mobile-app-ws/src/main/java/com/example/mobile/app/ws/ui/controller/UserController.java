7debfb94677cb216d31a2c2d9d7db83b1b4e0f0c
AvinashTiwari85 <qwe123kids@gmail.com>
2021-03-14 12:05:02 -0600

    no message

--- 22_Spring_boot_app/mobile-app-ws/src/main/java/com/example/mobile/app/ws/ui/controller/UserController.java
+++ 22_Spring_boot_app/mobile-app-ws/src/main/java/com/example/mobile/app/ws/ui/controller/UserController.java
@@ -5,6 +5,13 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("users")
 public class UserController {
+    @GetMapping()
+    public String getUsers(@RequestParam(value="page") int page,
+                           @RequestParam(value="limit") int limit){
+        return "get users was called with page " +page+ " limit " +limit ;
+    }
+
+
     @GetMapping(path="/{userId}")
     public String getUser(@PathVariable String userId){
         return "get user was userrid " + userId;
