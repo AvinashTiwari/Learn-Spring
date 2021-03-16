a9290507e4206fcdf70ceb93b2604d966e6e81a1
AvinashTiwari85 <qwe123kids@gmail.com>
2021-03-16 15:52:59 -0600

    no message

--- 22_Spring_boot_app/mobile-app-ws/src/main/java/com/example/mobile/app/ws/ui/controller/UserController.java
+++ 22_Spring_boot_app/mobile-app-ws/src/main/java/com/example/mobile/app/ws/ui/controller/UserController.java
@@ -1,20 +1,27 @@
 package com.example.mobile.app.ws.ui.controller;
 
+import com.example.mobile.app.ws.ui.model.response.UserRest;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("users")
 public class UserController {
     @GetMapping()
-    public String getUsers(@RequestParam(value="page") int page,
-                           @RequestParam(value="limit") int limit){
+    public String getUsers(@RequestParam(value="page", defaultValue = "1") int page,
+                           @RequestParam(value="limit", defaultValue = "50") int limit,
+                           @RequestParam(value="sort", defaultValue = "desc", required = false) String sort){
         return "get users was called with page " +page+ " limit " +limit ;
     }
 
 
     @GetMapping(path="/{userId}")
-    public String getUser(@PathVariable String userId){
-        return "get user was userrid " + userId;
+    public UserRest getUser(@PathVariable String userId){
+
+        UserRest returnValue = new UserRest();
+        returnValue.setFirstName("Avinash");
+        returnValue.setLastName("Tiwari");
+        returnValue.setEmailId("test@test.com");
+        return  returnValue;
     }
 
     @PostMapping
