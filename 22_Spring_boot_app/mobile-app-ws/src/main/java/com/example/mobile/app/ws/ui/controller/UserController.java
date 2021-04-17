bbd88b800d3d2351262d69508da0d0c05b4cf859
AvinashTiwari85 <qwe123kids@gmail.com>
2021-04-17 16:17:34 -0600

    no message

--- 22_Spring_boot_app/mobile-app-ws/src/main/java/com/example/mobile/app/ws/ui/controller/UserController.java
+++ 22_Spring_boot_app/mobile-app-ws/src/main/java/com/example/mobile/app/ws/ui/controller/UserController.java
@@ -4,6 +4,9 @@ import com.example.mobile.app.ws.execption.UserServiceException;
 import com.example.mobile.app.ws.ui.model.request.UpdateUserDetailsRequestModel;
 import com.example.mobile.app.ws.ui.model.request.UserDetailsRequestModel;
 import com.example.mobile.app.ws.ui.model.response.UserRest;
+import com.example.mobile.app.ws.userservice.UserService;
+import com.example.mobile.app.ws.userservice.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +22,8 @@ import java.util.UUID;
 @RequestMapping("users")
 public class UserController {
     Map<String, UserRest> users;
+    @Autowired
+    UserService userService;
     @GetMapping()
     public String getUsers(@RequestParam(value="page", defaultValue = "1") int page,
                            @RequestParam(value="limit", defaultValue = "50") int limit,
@@ -55,16 +60,8 @@ public class UserController {
             MediaType.APPLICATION_JSON_VALUE},  produces ={MediaType.APPLICATION_XML_VALUE,
             MediaType.APPLICATION_JSON_VALUE})
     public  ResponseEntity<UserRest> createUser(@Valid @RequestBody UserDetailsRequestModel userDetails){
-        UserRest returnValue = new UserRest();
-        returnValue.setFirstName(userDetails.getFirstName());
-        returnValue.setLastName(userDetails.getLastName());
-        returnValue.setEmailId(userDetails.getEmail());
-        String userId = UUID.randomUUID().toString();
-        returnValue.setUserId(userId);
-        if(users == null){
-            users = new HashMap<>();
-            users.put(userId,returnValue);
-        }
+        UserRest returnValue =  userService.createUser(userDetails);
+
         return  new ResponseEntity<>(returnValue,HttpStatus.OK);
     }
 
--- 22_Spring_boot_app/mobile-app-ws/src/main/java/com/example/mobile/app/ws/userservice/impl/UserServiceImpl.java
+++ 22_Spring_boot_app/mobile-app-ws/src/main/java/com/example/mobile/app/ws/userservice/impl/UserServiceImpl.java
@@ -1,12 +1,38 @@
 package com.example.mobile.app.ws.userservice.impl;
 
+import com.example.mobile.app.ws.shared.Utils;
 import com.example.mobile.app.ws.ui.model.request.UserDetailsRequestModel;
 import com.example.mobile.app.ws.ui.model.response.UserRest;
 import com.example.mobile.app.ws.userservice.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class UserServiceImpl implements UserService {
+
+    Map<String, UserRest> users;
+    Utils utils;
+
+    @Autowired
+    UserServiceImpl (Utils utils){
+        this.utils = utils;
+    }
+
     @Override
     public UserRest createUser(UserDetailsRequestModel userDetails) {
-        return null;
+
+        UserRest returnValue = new UserRest();
+        returnValue.setFirstName(userDetails.getFirstName());
+        returnValue.setLastName(userDetails.getLastName());
+        returnValue.setEmailId(userDetails.getEmail());
+        String userId = utils.generateUserId();
+        if(users == null){
+            users = new HashMap<>();
+            users.put(userId,returnValue);
+        }
+
+        return returnValue;
     }
 }
